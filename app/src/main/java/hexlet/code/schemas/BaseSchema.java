ce951e2be71f87a8fe79b14db88b70e917cd4572
arsen <arsenhan@mail.ru>
2023-12-09 04:41:32 +0300

    checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,43 +5,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class BaseSchema {
-    boolean isPositive = false;
-    public boolean required = false;
-    int rangeFrom;
-    int rangeUpTo;
-    int minLength = 0;
-    int mapSize = 0;
-    String contains = "";
-    final Map<String, BaseSchema> schemas = new HashMap<>();
+    protected boolean isPositive = false;
+    protected boolean required = false;
+    protected int rangeFrom;
+    protected int rangeUpTo;
+    protected int minLength = 0;
+    protected int mapSize = 0;
+    protected String contains = "";
+    protected final Map<String, BaseSchema> schemas = new HashMap<>();
 
-    public void sizeof(int sizeMap) {
+    public final void sizeof(int sizeMap) {
         this.mapSize = sizeMap;
     }
 
-    public void shape(Map<String, BaseSchema> schemasIn) {
+    public final void shape(Map<String, BaseSchema> schemasIn) {
         this.schemas.putAll(schemasIn);
     }
 
-    public BaseSchema positive() {
+    public final BaseSchema positive() {
         this.isPositive = true;
         return this;
     }
 
-    public void range(int minIn, int maxIn) {
+    public final void range(int minIn, int maxIn) {
         this.rangeFrom = minIn;
         this.rangeUpTo = maxIn;
     }
 
-    public void minLength(int minLengthIn) {
+    public final void minLength(int minLengthIn) {
         this.minLength = minLengthIn;
     }
 
-    public BaseSchema contains(String containsIn) {
+    public final BaseSchema contains(String containsIn) {
         this.contains = containsIn;
         return this;
     }
 
-    public BaseSchema required() {
+    public final BaseSchema required() {
         this.required = true;
         return this;
     }
